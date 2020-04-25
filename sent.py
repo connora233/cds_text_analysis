3eaf65d113086e8ef369047f5a37b14e05a5a61e
Connor Anderson <ca339@cornell.edu>
2020-04-25 14:17:25 -0400

    added check for job title near negative words

--- sent.py
+++ sent.py
@@ -3,7 +3,14 @@ import pickle
 with open('filings.txt', 'rb') as handle:
     filings = pickle.loads(handle.read())
 count = 0
+num_filings = 0
+added_filings = []
 tokenized_filings = {}
+change_words = ["hire", "appoint", "elect", "new",
+                "search", "fire", "resign", "resignation"]
+job_titles = ["ceo", "cfo", "chief executive officer",
+              "chief financial officer"]
+years = ["2016", "2017", "2018"]
 for key in filings:
     for reports in filings[key]:
         t = reports.lower()
@@ -13,12 +20,36 @@ for key in filings:
         else:
             tokenized_filings[key] = tokenized_filings[key] + [temp]
 for key in tokenized_filings:
+    yr = 0
     for lst in tokenized_filings[key]:
+        num_filings += 1
+        yr += 1
+        c = -15
+        b = -15
+        w = 0
         for word in lst:
-            if word == "ceo" or word == "cfo":
-                count += 1
+            if word in job_titles:
+                c = w
+                if w - b < 10:
+                    print(key)
+                    print(yr)
+                    if key not in added_filings:
+                        count += 1
+                        added_filings += [key]
+            if word in change_words:
+                b = w
+                if w - c < 10:
+                    print(key)
+                    print(yr)
+                    if key not in added_filings:
+                        count += 1
+                        added_filings += [key]
+            w += 1
+
 # Check unique number of filings that mention these strings, add "Chief
 # Executive Officer" and "Chief Financial Officer."
 #
 # Start comparing different years
 print(count)
+print(num_filings)
+print(count/num_filings)
